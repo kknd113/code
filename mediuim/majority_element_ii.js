384d70839efca782c19deef4b3eb848c76cdb46b
Adam Choi <choi0hun@gmail.com>
2016-04-05 23:57:34 -0700

    fix wrong solution

--- mediuim/majority_element_ii.js
+++ mediuim/majority_element_ii.js
@@ -2,22 +2,28 @@ var majorityElement = function(nums) {
     var buckets = [];
     var size = nums.length;
     if(size == 0) return [];
-    if(size <= 3) return nums;
+    if(size < 3) return Array.from(new Set(nums));
 
     for(var i=0; i < size; i++){
         var num = nums[i];
-        if(!buckets[0] || buckets[0].count === 0){
+        if(!buckets[0]){
             buckets[0] = { number: num, count: 1 };
         }
         else{
             if(buckets[0].number === num) buckets[0].count++;
             else {
-                buckets[0].count--;
-                if(buckets[1] && buckets[1].count > 0){
-                    if(buckets[1].number === num) buckets[1].count++;
-                    else buckets[1].count--;
+              if(!buckets[1]) buckets[1] = { number:num, count:1 };
+              else if(buckets[1].number === num) buckets[1].count++;
+              else{
+                if(buckets[0].count === 0)
+                  buckets[0] = {number: num, count: 1 };
+                else if(buckets[1].count === 0)
+                  buckets[1] = {number: num, count: 1 };
+                else{
+                  buckets[0].count--;
+                  buckets[1].count--;
                 }
-                else buckets[1] = { number:num, count:1 };
+              }
             }
         }
     }
