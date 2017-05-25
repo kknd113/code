81a62dc7bbb33f3a3043a6aedcc1194d75f52c44
chris hyun ho kim <chrishyunhokim@gmail.com>
2017-05-25 13:44:48 -0700

    license key formatting

--- mediuim/license_key_formatting.rb
+++ mediuim/license_key_formatting.rb
@@ -1,21 +1,20 @@
 # @param {String} s
 # @param {Integer} k
 # @return {String}
+
 def license_key_formatting(s, k)
-  s.gsub!('-','')
+  s = s.gsub('-','').upcase
   head_len = s.length % k
   
-  puts s
-  puts head_len
-  
   index = head_len
-  new_s = s[0...head_len]
+  new_s = s[0...head_len] + '-'
   while index < s.length
-    new_s << s[index...index+4].map(&upcase)
+    new_s << s[index...index+k]
     new_s << '-'
-    index+=4
+    index+=k
   end
   
   new_s[new_s.length-1] = '' if new_s[new_s.length-1] == '-'
+  new_s[0] = '' if new_s[0] == '-'
   new_s
 end
